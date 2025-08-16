e684650657f78368c83172205cf7e397a2830e0a
RarogCmex <denis7774@gmail.com>
2025-08-17 01:10:50 +0500

    Attempt to fix regulator building

--- drivers/regulator/mtk-dvfsrc-regulator-trace.h
+++ drivers/regulator/mtk-dvfsrc-regulator-trace.h
@@ -6,6 +6,10 @@
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM mtk_pm_qos_regulator
 
+/* These must be defined *before* include guard */
+#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_FILE mtk-dvfsrc-regulator-trace
+
 #if !defined(_TRACE_MTK_QOS_REGULATOR_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _TRACE_MTK_QOS_REGULATOR_H
 
@@ -16,43 +20,31 @@
 #define TPS(x)  tracepoint_string(x)
 
 DECLARE_EVENT_CLASS(mtk_pm_qos_request,
-
-	TP_PROTO(int mtk_pm_qos_class, s32 value, const char *owner),
-
-	TP_ARGS(mtk_pm_qos_class, value, owner),
-
-	TP_STRUCT__entry(
-		__field(int, mtk_pm_qos_class)
-		__field(s32, value)
-		__array(char, owner, 40)
-	),
-
-	TP_fast_assign(
-		__entry->mtk_pm_qos_class = mtk_pm_qos_class;
-		__entry->value = value;
-		strncpy(__entry->owner, owner, 39);
-	),
-
-	TP_printk("pm_qos_class=%d value=%d owner=%s",
-		  __entry->mtk_pm_qos_class,
-		  __entry->value,
-		   __entry->owner)
+					TP_PROTO(int mtk_pm_qos_class, s32 value, const char *owner),
+					TP_ARGS(mtk_pm_qos_class, value, owner),
+					TP_STRUCT__entry(
+						__field(int, mtk_pm_qos_class)
+						__field(s32, value)
+						__array(char, owner, 40)
+					),
+					TP_fast_assign(
+						__entry->mtk_pm_qos_class = mtk_pm_qos_class;
+						__entry->value = value;
+						strncpy(__entry->owner, owner, 39);
+						__entry->owner[39] = '\0';
+					),
+					TP_printk("pm_qos_class=%d value=%d owner=%s",
+							  __entry->mtk_pm_qos_class,
+			   __entry->value,
+			   __entry->owner)
 );
 
 DEFINE_EVENT(mtk_pm_qos_request, mtk_pm_qos_update_request,
-
-	TP_PROTO(int mtk_pm_qos_class, s32 value, const char *owner),
-
-	TP_ARGS(mtk_pm_qos_class, value, owner)
+			 TP_PROTO(int mtk_pm_qos_class, s32 value, const char *owner),
+			 TP_ARGS(mtk_pm_qos_class, value, owner)
 );
 
 #endif /* _TRACE_MTK_QOS_REGULATOR_H */
-/* This part must be outside protection */
-#undef TRACE_INCLUDE_PATH
-#define TRACE_INCLUDE_PATH .
 
-#undef TRACE_INCLUDE_FILE
-#define TRACE_INCLUDE_FILE mtk-dvfsrc-regulator-trace
-
-/* This part must be outside protection */
+/* This must be outside the header guard */
 #include <trace/define_trace.h>
